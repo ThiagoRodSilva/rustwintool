081f67e6ac78194381f132af557d0db506b53835
ThiagoRodSilva <trsilva15544@gmail.com>
2026-09-04 12:03:27 -0300

    Adiciona comando para atualizar programas instalados com Winget

--- src/shell.rs
+++ src/shell.rs
@@ -1,4 +1,5 @@
 pub const SHELL: &[(&str, &str)] = &[
+        ("Winget (Atualizar programas instalados)", "winget upgrade --all"),
         ("sfc", "sfc /scannow"),
         ("chkdsk", "chkdsk C: /f /r"),
         ("DISM", "dism /online /cleanup-image /restorehealth"),
